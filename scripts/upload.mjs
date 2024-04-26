b9e6f50d486e7997e92059d468c2e3310cb5e6f0
orekentaro <kntru0218gj@gmail.com>
2024-04-26 12:45:04 +0900

    :lipstick: lint

--- scripts/upload.mjs
+++ scripts/upload.mjs
@@ -20,7 +20,6 @@ if (KINTONE_BASIC_AUTH_USERNAME && KINTONE_BASIC_AUTH_PASSWORD) {
   command += ` --basic-auth-username ${KINTONE_BASIC_AUTH_USERNAME} --basic-auth-password ${KINTONE_BASIC_AUTH_PASSWORD}`;
 }
 command += " --watch --waiting-dialog-ms 3000";
-console.log(command);
 
 exec(command, (error, stdout, stderr) => {
   if (error) {
