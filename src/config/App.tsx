3ac8adb4b9ef9772db56538773744ee7f13a4d1d
orekentaro <kntru0218gj@gmail.com>
2024-05-09 23:22:27 +0900

    :+1: メインページを動くようにした

--- src/config/App.tsx
+++ src/config/App.tsx
@@ -1,13 +1,72 @@
-import { FC } from "react";
-import type { PluginId } from "../types/config";
+import { ChangeEventHandler, FC, useEffect, useState } from "react";
+import type { ConfigState, PluginId } from "../types/config";
 import Header from "../components/Header";
-import TextField from "../components/atoms/TextField";
-import DropdownWithFormInfo from "../components/atoms/DropdownWithFormInfo";
-import ButtonGroup from "../components/molecules/ButtonGroup";
 import BackGroundColorSpace from "../components/atoms/BackGroundColorSpace";
+import {
+  RecoilState,
+  RecoilValue,
+  useRecoilCallback,
+  useRecoilState,
+} from "recoil";
+import { configState } from "../store/config";
+import TextField from "../components/molecules/TextField";
+import Dropdown, { DropDownValue } from "../components/molecules/Dropdown";
+import ButtonGroup from "../components/molecules/ButtonGroup";
+import { backPage, getFormInfo } from "../utils/kintone";
 
 const App: FC<PluginId> = ({ PLUGIN_ID }) => {
-  console.log(PLUGIN_ID);
+  const [config, setConfig] = useRecoilState(configState);
+  const [values, setValues] = useState<DropDownValue[]>([]);
+  const getConfig = useRecoilCallback(
+    ({ snapshot }) =>
+      (
+        state: RecoilState<ConfigState> | RecoilValue<ConfigState>
+      ): ConfigState => {
+        return snapshot.getLoadable(state).contents;
+      },
+    []
+  );
+  const handleTextFieldChange: ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    const newName = event.target.value;
+    setConfig((prevConfig) => ({
+      ...prevConfig,
+      name: newName !== undefined ? newName : prevConfig.name,
+    }));
+  };
+
+  const handleDropdownChange: ChangeEventHandler<HTMLSelectElement> = (
+    event
+  ) => {
+    const newField = event.target.value;
+    setConfig((prevConfig) => ({
+      ...prevConfig,
+      field: newField !== undefined ? newField : prevConfig.field,
+    }));
+  };
+  const setKintoneConfig = () => {
+    const fixConfig = getConfig(configState);
+    kintone.plugin.app.setConfig(fixConfig);
+  };
+
+  useEffect(() => {
+    const kintoneConfig = kintone.plugin.app.getConfig(PLUGIN_ID);
+    const getValues = async () => {
+      const formInfo = await getFormInfo(["SINGLE_LINE_TEXT"]);
+      const v = formInfo.map((i) => {
+        return {
+          key: i.code,
+          value: i.label,
+        };
+      });
+      setValues(v);
+    };
+    getValues();
+    setConfig(kintoneConfig);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
       <Header title={"Plugin Sample!!!"} subTitle={"Write plugin detail"} />
@@ -15,19 +74,22 @@ const App: FC<PluginId> = ({ PLUGIN_ID }) => {
         <TextField
           className="pb-2"
           label="Text Field"
-          onChange={(event) => console.log(event)}
+          onChange={handleTextFieldChange}
+          value={config.name}
+          error="error!!!"
         />
-        <DropdownWithFormInfo
-          className="pb-2"
-          fieldCode={["DATE", "SINGLE_LINE_TEXT"]}
+        <Dropdown
+          value={config.field}
+          items={values}
+          onChange={handleDropdownChange}
         />
       </BackGroundColorSpace>
       <ButtonGroup
         className="justify-content-between"
-        cancelText="戻る"
-        confirmText="登録"
-        onConfirmClick={(event) => console.log(event)}
-        onCancelClick={(event) => console.log(event)}
+        cancelText="キャンセル"
+        confirmText="送信"
+        onConfirmClick={setKintoneConfig}
+        onCancelClick={backPage}
       />
     </>
   );
