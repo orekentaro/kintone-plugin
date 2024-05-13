4a0a40ba38c7862eb63fc86e4ad3bb9a5991aadc
orekentaro <kntru0218gj@gmail.com>
2024-05-13 23:57:24 +0900

    :lipstick: 保存の連打防止

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,22 +1,40 @@
 import { exec } from "child_process";
 import chokidar from "chokidar";
 
+let isScriptRunning = false;
+let timeoutId = null;
+
 const directoryToWatch = "./src";
 const watcher = chokidar.watch(directoryToWatch);
 
 watcher.on("change", (path) => {
-  console.log(`\x1b[35mchange file: ${path}`);
-  console.log("\x1b[32mstart");
-  exec("npm run move:static", () => {
-    exec("npm run webpack", () => {
-      exec("npm run kintone", () => {
-        console.log("\x1b[36muploading...");
-        exec("npm run upload", () => {
-          console.log("\x1b[32mdone");
+  if (!isScriptRunning) {
+    isScriptRunning = true;
+    // 1. スクリプトの実行中に新しいファイルの変更を無視する
+    clearTimeout(timeoutId);
+    // 2. スクリプトの実行中に新しい変更があれば、以前の実行をキャンセルして新しい実行を予約する
+    timeoutId = setTimeout(() => {
+      console.log(`\x1b[35mChange file: ${path}`);
+      exec("npm run move:static", () => {
+        console.log("\x1b[32mStart");
+      });
+      exec("npm run webpack", () => {
+        exec("npm run kintone", () => {
+          console.log("\x1b[36mUploading...");
+          exec("npm run upload", () => {
+            console.log("\x1b[32mDone");
+            isScriptRunning = false;
+          });
         });
       });
-    });
-  });
+    }, 1000);
+  } else {
+    // 3. スクリプトの実行中に新しい変更があれば、待機時間をリセットしてからスクリプトの実行を再スケジュールする
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      isScriptRunning = false;
+    }, 5000);
+  }
 });
 
 watcher.on("error", (error) => {
