8635739ec97bdd3c77244f433a4e320fd900a173
orekentaro <kntru0218gj@gmail.com>
2024-05-13 18:57:35 +0900

    :+1: vite last

--- src/desktop/desktop.tsx
+++ src/desktop/desktop.tsx
@@ -1,6 +1,9 @@
-((PLUGIN_ID) => {
-  kintone.events.on("app.record.index.show", (event) => {
-    console.log(PLUGIN_ID);
-    return event;
-  });
-})(kintone.$PLUGIN_ID);
+import { makeRandomString } from "../utils/make";
+
+const PLUGIN_ID = kintone.$PLUGIN_ID;
+kintone.events.on("app.record.index.show", (event) => {
+  const kintoneConfig = kintone.plugin.app.getConfig(PLUGIN_ID);
+  console.log(makeRandomString());
+  console.log(kintoneConfig);
+  return event;
+});
